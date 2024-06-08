b113241930ce39dbaa1a3e688be341a9c2fe74d2
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-06-08 09:55:27 +0100

    logout feature not functioning

--- api/server.ts
+++ api/server.ts
@@ -11,11 +11,12 @@ import { v2 as cloudinary } from "cloudinary";
 import connectMongoDB from "./db/connectMongoDB";
 
 dotenv.config();
+
 const app = express();
 
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL,
+		origin: process.env.FRONTEND_URL || "https://twitter-appp.vercel.app",
 		credentials: true,
 	}),
 );
@@ -29,6 +30,7 @@ cloudinary.config({
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "5mb" }));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
